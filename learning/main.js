3cdd201a83da2d33cdb791b6cf13babf8b5d8b97
stivman <stiv.m@mail.ru>
2019-04-11 02:24:57 +0700

    добавил файл

---
+++ learning/main.js
@@ -0,0 +1,10 @@
+var money = prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+var income = [];
+var appData = { money, 
+                timeData: time, 
+                expenses, 
+                optionalExpenses, 
+                income, 
+                saving: false
+            };
--- learning/test.js
+++ learning/test.js
@@ -1,5 +1,29 @@
-// выражение X*Y
-let x = prompt ("Введите X");
-let y = prompt ("Введите Y");
+let num = 50;
 
-alert ("получилось " + x*y);
+if (num<49) {
+    console.log("Неверно")
+} else if (num>100) {
+    console.log("Много")
+} else {
+    console.log("Верно1")
+}
+
+(num == 50) ? console.log("Верно2") : console.log("Неверно");
+
+switch (num) {
+    case num < 49:
+        console.log("Неверно");
+        break;
+    case num > 100:
+        console.log("Много");
+        break;
+    case num > 80:
+        console.log("Всй ещй много");
+        break;
+    case 50:
+        console.log("Верно3");
+        break;
+    default:
+        console.log("Что-то пошло не так");
+        break;
+}
