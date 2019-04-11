718f7b704a1c81367946addd1914f4224ff5d744
stivman <stiv.m@mail.ru>
2019-04-11 22:30:58 +0700

    exercise1

--- learning/main.js
+++ learning/main.js
@@ -1,10 +1,35 @@
-var money = prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату в формате YYYY-MM-DD");
-var income = [];
-var appData = { money, 
+let money = +prompt("Ваш бюджет на месяц?", ''),
+     time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = { budget: money, 
                 timeData: time, 
-                expenses, 
-                optionalExpenses, 
-                income, 
-                saving: false
+                expenses: {}, 
+                optionalExpenses: {}, 
+                income: [], 
+                savings: false
             };
+
+for (let i = 0; i<2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдётся?', '');
+    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+}
+
+appData.moneyPerDay = appData.budget/30;
+
+alert('Ежедневный бюджет '+ appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("высокий уровень достатка");
+} else {
+    console.log("ОШИБКА");
+}
