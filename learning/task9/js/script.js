e38c6ec2e64bbf4e2ad57fa85deb491b646b7608
stivman <stiv.m@mail.ru>
2019-05-09 18:38:22 +0700

    Task10 add timer

--- learning/task9/js/script.js
+++ learning/task9/js/script.js
@@ -21,7 +21,8 @@
             tabContent[b].classList.add('show');
         }
     }
-//  вешаем обработчик событий на родителя кнопок, определяем, на какой кнопке был был клик,  
+//  вешаем обработчик событий на родителя кнопок, определяем, 
+//  на какой кнопке был был клик,  
 //  и показываем тат таб, индекс которого соответствует номеру кнопки.
     info.addEventListener('click', function(event) {
         let target = event.target;
@@ -36,4 +37,54 @@
             }
         }
     });
+
+
+    // Timer
+    let deadline = '2019-06-03';
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/(1000*60*60)));
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+            
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+           let t = getTimeRemaining(deadline);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            //  ставлю ноль перед цифрой
+            if(t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            }
+            if(t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            }
+            if(t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            }
+
+            //  остановить таймер, если отчёт закончен
+            if(t.total <=0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('timer', deadline);
+
  });
